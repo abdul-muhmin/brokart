e18f0674510950b502376afe51e22177e1fa59b2
abdul-muhmin <abdulmuhminshakeer@gmail.com>
2025-05-16 11:24:03 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,13 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager  
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-# Setup (headless for GitHub Actions)
 options = Options()
 options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
 driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+
 
 try:
     # Step 1: Go to login page
