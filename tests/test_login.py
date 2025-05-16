ac4588f7a5074b84377701f980f8cc594cb68b82
abdul-muhmin <abdulmuhminshakeer@gmail.com>
2025-05-16 15:17:15 +0530

    Update test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,6 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+
 @pytest.fixture(scope="module")
 def driver():
     # Setup Chrome options
